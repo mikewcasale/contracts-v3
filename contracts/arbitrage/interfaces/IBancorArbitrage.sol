7997666a0cd23a7d1ba2e1a5ba9a0f50ce52e61c
mikewcasale <mike.w.casale@gmail.com>
2023-01-02 19:40:15 -0800

    Update IBancorArbitrage.sol

--- contracts/arbitrage/interfaces/IBancorArbitrage.sol
+++ contracts/arbitrage/interfaces/IBancorArbitrage.sol
@@ -3,40 +3,128 @@ pragma solidity 0.8.13;
 
 import { IUpgradeable } from "../../utility/interfaces/IUpgradeable.sol";
 import { Token } from "../../token/Token.sol";
+import { IBancorNetwork, IFlashLoanRecipient } from "../network/interfaces/IBancorNetwork.sol";
+import { ISwapRouter } from "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 
-struct PositionMigration {
-    uint256 amountA;
-    uint256 amountB;
+struct TradeParams {
+    Token sourceToken;
+    Token targetToken;
+    uint256 sourceAmount;
+    uint256 minReturnAmount;
+    uint256 deadline;
+    uint exchangeId;
 }
+
+interface IUniswapV3Router is ISwapRouter {
+    function refundETH() external payable;
+}
+
+interface IUniswapV2Router02 {
+    function swapExactTokensForTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts);
+    function swapTokensForExactTokens(
+        uint amountOut,
+        uint amountInMax,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts);
+    function swapExactETHForTokens(
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external payable returns (uint[] memory amounts);
+    function swapTokensForExactETH(
+        uint amountOut,
+        uint amountInMax,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts);
+    function swapExactTokensForETH(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts);
+    function swapETHForExactTokens(
+        uint amountOut,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external payable returns (uint[] memory amounts);
+}
+
 
 interface IBancorArbitrage is IUpgradeable {
     /**
-     * @dev migrates funds from a Uniswap v2 pair into a bancor v3 pool and returns the deposited amount for each token
-     * in the same order as stored in Uniswap's pair, or 0 for unsupported tokens (unsupported tokens will be
-     * transferred to the caller)
-     *
-     * requirements:
-     *
-     * - the caller must have approved the pair to transfer the liquidity on its behalf
+     * @dev trades on UniswapV2 and returns the token1 amount
      */
-    function migrateUniswapV2Position(
-        Token token0,
-        Token token1,
-        uint256 amount
-    ) external returns (PositionMigration memory);
+    function tradeUniswapV2(
+        Token sourceToken,
+        Token targetToken,
+        uint256 sourceAmount,
+        uint256 minReturnAmount,
+        uint256 deadline,
+        address beneficiary
+    ) external returns (uint256);
 
     /**
-     * @dev migrates funds from a SushiSwap pair into a bancor v3 pool and returns the deposited amount for each token
-     * in the same order as stored in Uniswap's pair, or 0 for unsupported tokens (unsupported tokens will be
-     * transferred to the caller)
-     *
-     * requirements:
-     *
-     * - the caller must have approved the pair to transfer the liquidity on its behalf
+     * @dev trades on UniswapV3 and returns the token1 amount
      */
-    function migrateSushiSwapPosition(
-        Token token0,
-        Token token1,
-        uint256 amount
-    ) external returns (PositionMigration memory);
+    function tradeUniswapV3(
+        Token sourceToken,
+        Token targetToken,
+        uint256 sourceAmount,
+        uint256 minReturnAmount,
+        uint256 deadline,
+        address beneficiary
+    ) external returns (uint256);
+
+    /**
+     * @dev trades on SushiSwap and returns the token1 amount
+     */
+    function tradeSushiSwap(
+        Token sourceToken,
+        Token targetToken,
+        uint256 sourceAmount,
+        uint256 minReturnAmount,
+        uint256 deadline,
+        address beneficiary
+    ) external returns (uint256);
+
+    /**
+     * @dev trades on BancorV3 and returns the token1 amount
+     */
+    function tradeBancorV3(
+        Token sourceToken,
+        Token targetToken,
+        uint256 sourceAmount,
+        uint256 minReturnAmount,
+        uint256 deadline
+    ) external returns (uint256);
+
+    /**
+     * @dev takes out a flash loan of BNT on BancorV3
+     */
+    function takeFlashLoan(
+        Token token,
+        uint256 amount,
+        IFlashLoanRecipient recipient,
+        bytes calldata data
+    ) external;
+
+    /**
+     * @dev executes the arbitrage trades according to a list of routes, return the amount of profits sent to the
+     * caller and burns the remaining BNT amount
+     */
+    function execute(TradeParams[] memory _trades
+    ) external payable returns (uint256);
 }
